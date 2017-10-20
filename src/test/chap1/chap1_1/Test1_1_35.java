7d3069c175f7c1be221801d79c36a5ed55ff8204
BEFORE-4 <836735557@qq.com>
2017-10-20 10:08:20 +0800

    1.1.35题

--- src/test/chap1/chap1_1/Test1_1_35.java
+++ src/test/chap1/chap1_1/Test1_1_35.java
@@ -1,4 +1,40 @@
 package test.chap1.chap1_1;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Test1_1_35 {
+    public static void main(String[] args) {
+        double[] theory = getNum();
+        //1 000 000 次基本上达到小数点后三位吻合
+        double[] fact = simulate(1000000);
+        for (int i = 2; i < theory.length; i++) {
+            System.out.println(fact[i] + "\t" +theory[i] );
+        }
+    }
+    static double[] getNum(){
+        int SIDES = 6;
+        double[] dist = new double[2 * SIDES + 1];
+        for (int i = 1; i <= SIDES ; i++) {
+            for (int j = 1; j <= SIDES; j++) {
+                dist[i + j] += 1.0;
+            }
+        }
+        for (int k = 2; k <= 2 * SIDES ; k++) {
+            dist[k] /= 36.0;
+        }
+        return dist;
+    }
+    static double[] simulate(int N){
+        int SIDES = 6;
+        double[] dist = new double[2 * SIDES + 1];
+        for (int i = 0; i < N; i++) {
+            int num1 = StdRandom.uniform(1, 7);
+            int num2 = StdRandom.uniform(1, 7);
+            dist[num1 + num2] += 1.0;
+        }
+        for (int i = 2; i < dist.length; i++) {
+            dist[i] /= N;
+        }
+        return dist;
+    }
 }
