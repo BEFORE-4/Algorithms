4357af32cb60a2f31868d471a1b6c8be9573e866
BEFORE-4 <836735557@qq.com>
2018-07-31 15:56:38 +0800

    ThreeSum防止int型溢出，加long即可

--- src/txt/chap1/chap1_4/ThreeSum.java
+++ src/txt/chap1/chap1_4/ThreeSum.java
@@ -28,7 +28,7 @@ public class ThreeSum {
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
                 for (int k = j + 1; k < N; k++) {
-                    if (a[i] + a[j] + a[k] == 0)
+                    if ((long)a[i] + a[j] + a[k] == 0)
                         cnt++;
                 }
             }
